b6ed4f07858b9755b46c4e955fbec341d7c7a60b
Susan Dreher <sdreher@gmail.com>
2020-12-05 11:37:27 -0500

    Redo the video loading

--- js/controls.js
+++ js/controls.js
@@ -1,66 +1,72 @@
-var tag = document.createElement('script');
+(function() {
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    // Load the YouTube iFrame API
+    let tag = document.createElement('script');
 
-var thePlayer;
-function onYouTubeIframeAPIReady() {
-    console.log('YouTubeIframeAPIReady');
-    thePlayer = new YT.Player('the-video', {
-        videoId: overviewVideo,
-    });
-}
+    tag.src = 'https://www.youtube.com/iframe_api';
+    let firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-function loadVideo(videoId, start) {
-    
-}
+    let thePlayer;
 
-jQuery(document).ready(function() {
+    // This ready function will be called when the iFrame load is complete 
+    // eslint-disable-next-line no-unused-vars
+    function onYouTubeIframeAPIReady() {
+        /* eslint-disable no-undef */
+        thePlayer = new YT.Player('the-video', {
+            videoId: overviewVideo,
+        });
+        /* eslint-enable no-undef */
+    }
 
-    jQuery('.list-group-item').click(function(evt) {
-        thePlayer.stopVideo();
-        jQuery('.list-group-item').removeClass('active');
+    jQuery(document).ready(function() {
 
-        const $elt = jQuery(evt.currentTarget);
-        $elt.addClass('active');
+        jQuery('.list-group-item').click(function(evt) {
+            thePlayer.stopVideo();
+            jQuery('.list-group-item').removeClass('active');
 
-        let videoId = $elt.data('target')
-        if (videoId) {
-            thePlayer.loadVideoById(videoId, $elt.data('start'));
-        }
-    });
+            const $elt = jQuery(evt.currentTarget);
+            $elt.addClass('active');
 
-    jQuery('.nav-link').click(function(evt) {
-        thePlayer.stopVideo();
-        jQuery('.list-group-item').removeClass('active');
-
-        // If the nav-link specifies a video, use that target & start
-        const $elt = jQuery(evt.currentTarget);
-
-        let videoId = $elt.data('target');
-        if (videoId) {
-            thePlayer.cueVideoById(videoId, 0);
-        } else {
-            // Grab the first listed video and cue it up
-            let $list;
-            let ref = $elt.attr('href');
-            if ($elt.hasClass('primary')) {
-                let $nav = jQuery(ref).find('.nav-link.secondary.active');
-                $list = jQuery($nav.attr('href'));
-            } else {
-                $list = jQuery(ref);
+            let videoId = $elt.data('video');
+            if (videoId) {
+                thePlayer.loadVideoById(videoId, $elt.data('start'));
             }
-            const $item = $list.find('.list-group-item').first();
+        });
 
-            if ($item.length) {
-                $item.addClass('active');
-                let videoId = $item.data('target');
-                let start = $item.data('start');
-                if (videoId) {
-                    thePlayer.cueVideoById(videoId, $elt.data('start'));
+        jQuery('.nav-link').click(function(evt) {
+            thePlayer.stopVideo();
+            jQuery('.list-group-item').removeClass('active');
+
+            // If the nav-link specifies a video, use that video
+            const $elt = jQuery(evt.currentTarget);
+
+            let videoId = $elt.data('video');
+            if (videoId) {
+                thePlayer.cueVideoById(videoId, 0);
+            } else {
+                // Grab the first listed video and cue it up
+                let $list;
+                let ref = $elt.attr('href');
+                if ($elt.hasClass('primary')) {
+                    // Get the video list in the active secondary nav
+                    let $nav = jQuery(ref).find('.nav-link.secondary.active');
+                    $list = jQuery($nav.attr('href'));
+                } else {
+                    // Get the video list (this is the active secondary nav!)
+                    $list = jQuery(ref);
+                }
+
+                const $item = $list.find('.list-group-item').first();
+                if ($item.length) {
+                    $item.addClass('active');
+                    let videoId = $item.data('video');
+                    if (videoId) {
+                        // Use cue here rather than seek to show but not play
+                        thePlayer.cueVideoById(videoId, $elt.data('start'));
+                    }
                 }
             }
-        }
+        });
     });
 });
